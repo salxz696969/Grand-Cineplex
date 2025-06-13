1a6bba2efb7c232788a5291da8e8dec6767de098
Chanraksa Ory <OCraksa@gmail.com>
2025-06-13 15:14:44 +0700

    changed db

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import pool from "./utils/db";
 
 const app: Express = express();
 
@@ -9,7 +10,13 @@ app.use(express.json());
 
 // Basic route
 app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Welcome to the TypeScript Express Server!' });
+    pool.query('SELECT * FROM movies', (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+
+      res.json("executed query");
+    })
 });
 
 export default app;
---
+++ server/src/utils/db.ts
@@ -0,0 +1,15 @@
+import {Pool} from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL, // recommended
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+export default pool;
+
+
